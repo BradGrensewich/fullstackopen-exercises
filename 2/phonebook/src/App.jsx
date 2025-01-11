f7a098e697b8530ec18cf64ae8f4be76373f03c8
BradGrensewich <bradgrensewich@gmail.com>
2025-01-11 14:32:19 +0700

    exercise 2.9

--- 2/phonebook/src/App.jsx
+++ 2/phonebook/src/App.jsx
@@ -1,11 +1,37 @@
 import { useState } from 'react';
 
+const PersonItem = ({ person }) => {
+	return (
+		<li>
+			{person.name} {person.number}
+		</li>
+	);
+};
+
+const PersonList = ({ persons, searchFilter }) => {
+	const caseLessFilter = searchFilter.toLowerCase();
+	const filteredPersons = persons.filter((person) =>
+		person.name.toLowerCase().includes(caseLessFilter)
+	);
+	return (
+		<ul>
+			{filteredPersons.map((person) => (
+				<PersonItem key={person.id} person={person} />
+			))}
+		</ul>
+	);
+};
+
 const App = () => {
 	const [persons, setPersons] = useState([
-		{ name: 'Arto Hellas', number: '1234567890' },
+		{ name: 'Arto Hellas', number: '040-123456', id: 1 },
+		{ name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+		{ name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+		{ name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
 	]);
 	const [newName, setNewName] = useState('');
 	const [newNumber, setNewNumber] = useState('');
+	const [searchFilter, setSearchFilter] = useState('');
 
 	const addPerson = (event) => {
 		event.preventDefault();
@@ -13,7 +39,13 @@ const App = () => {
 			alert(`${newName} is already added to phonebook`);
 			return;
 		}
-		setPersons(persons.concat({ name: newName, number: newNumber }));
+		setPersons(
+			persons.concat({
+				name: newName,
+				number: newNumber,
+				id: persons.length + 1,
+			})
+		);
 		setNewName('');
 		setNewNumber('');
 	};
@@ -24,10 +56,18 @@ const App = () => {
 	const updateNewNumber = (event) => {
 		setNewNumber(event.target.value);
 	};
+	const updateSearchFilter = (event) => {
+		setSearchFilter(event.target.value);
+	};
 
 	return (
 		<div>
 			<h2>Phonebook</h2>
+			<div>
+				filter shown with{' '}
+				<input onChange={updateSearchFilter} value={searchFilter} />
+			</div>
+			<h2>Numbers</h2>
 			<form onSubmit={addPerson}>
 				<div>
 					name: <input onChange={updateNewName} value={newName} />
@@ -41,13 +81,7 @@ const App = () => {
 				</div>
 			</form>
 			<h2>Numbers</h2>
-			<ul>
-				{persons.map((person) => (
-					<li key={person.name}>
-						{person.name} {person.number}
-					</li>
-				))}
-			</ul>
+			<PersonList persons={persons} searchFilter={searchFilter} />
 		</div>
 	);
 };
