e3817c9788b1509e77af8a363edc372e5da45f4a
BradGrensewich <bradgrensewich@gmail.com>
2025-02-01 14:13:22 +0700

    exercise 4.4

--- 4/bloglist/tests/list_helper.test.js
+++ 4/bloglist/tests/list_helper.test.js
@@ -1,11 +1,35 @@
 const { test, describe } = require('node:test');
 const assert = require('node:assert');
 
+const dummyBlogs = require('./dummyData');
+
 const listHelper = require('../utils/list_helper');
 
 describe('dummy', () => {
   test('returns one', () => {
     const blogs = [];
     assert.strictEqual(listHelper.dummy(blogs), 1);
+  });
+});
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0,
+    },
+  ];
+  test('when list has only one blog, equals the likes of that', () => {
+    assert.strictEqual(listHelper.totalLikes(listWithOneBlog), 5);
+  });
+  test('when list has many blogs, equals total likes', () => {
+    assert.strictEqual(listHelper.totalLikes(dummyBlogs), 36);
+  });
+  test('when list is empty, equals zero', () => {
+    assert.strictEqual(listHelper.totalLikes([]), 0);
   });
 });
--- 4/bloglist/utils/list_helper.js
+++ 4/bloglist/utils/list_helper.js
@@ -1,5 +1,15 @@
 const dummy = (blogs) => {
+  if (blogs) {
+    return 1;
+  }
   return 1;
 };
 
-module.exports = { dummy };
+const totalLikes = (blogs) => {
+  const total = blogs.reduce((accumulator, blog) => {
+    return accumulator + blog.likes;
+  }, 0);
+  return total;
+};
+
+module.exports = { dummy, totalLikes };
