b1c09ef60d87c0c12d22a42572905e8802e8b9bc
BradGrensewich <bradgrensewich@gmail.com>
2025-04-08 16:42:06 +0700

    exercise 5.22

--- 5/bloglist-tests/tests/bloglist_app.spec.js
+++ 5/bloglist-tests/tests/bloglist_app.spec.js
@@ -1,5 +1,6 @@
 const { describe, beforeEach, test, expect } = require('@playwright/test');
 const { login, createBlog } = require('./helpers');
+const { request } = require('http');
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
@@ -38,32 +39,50 @@ describe('Blog app', () => {
     });
 
     test('a new blog can be created', async ({ page }) => {
-      await createBlog(page, 'test title', 'test author', 'test url')
-      const blog = await page.getByTestId('blog')
+      await createBlog(page, 'test title', 'test author', 'test url');
+      const blog = await page.getByTestId('blog');
       await expect(blog.getByText('test title')).toBeVisible();
     });
 
     describe('and a blog has been created', () => {
-        beforeEach(async ({page}) => {
-            await createBlog(page, 'test title', 'test author', 'test url')
-        })
+      beforeEach(async ({ page }) => {
+        await createBlog(page, 'test title', 'test author', 'test url');
+      });
 
-        test('it can be liked', async ({page}) => {
-            await page.getByRole('button', {name: 'show'}).click()
-            await page.getByRole('button', {name: 'like'}).click()
-            await expect(page.getByText('likes: 1')).toBeVisible()
-        })
+      test('it can be liked', async ({ page }) => {
+        await page.getByRole('button', { name: 'show' }).click();
+        await page.getByRole('button', { name: 'like' }).click();
+        await expect(page.getByText('likes: 1')).toBeVisible();
+      });
 
-        test.only('it can be deleted by its owner', async ({page}) => {
-            await page.getByRole('button', {name: 'show'}).click()
-            await page.getByRole('button', {name: 'remove'}).click()
-            
-            await expect(page.getByText('blog deleted')).toBeVisible()
-            await expect(page.getByText('test title')).not.toBeVisible();     
+      test('it can be deleted by its owner', async ({ page }) => {
+        await page.getByRole('button', { name: 'show' }).click();
+        await page.getByRole('button', { name: 'remove' }).click();
 
-        })
-    })
+        await expect(page.getByText('blog deleted')).toBeVisible();
+        await expect(page.getByText('test title')).not.toBeVisible();
+      });
+
+      test.only('it cant be deleted by a second user', async ({
+        page,
+        request,
+      }) => {
+        await request.post('http://localhost:3001/api/users', {
+          data: {
+            username: 'user2',
+            password: 'password',
+            name: 'User2',
+          },
+        });
+        await page.getByRole('button', { name: 'Logout' }).click();
+        await login(page, 'user2', 'password');
+        await page.getByRole('button', { name: 'show' }).click();
+
+        await expect(page.getByText('test url')).toBeVisible();
+        await expect(
+          page.getByRole('button', { name: 'delete' }),
+        ).not.toBeVisible();
+      });
+    });
   });
-
-
 });
